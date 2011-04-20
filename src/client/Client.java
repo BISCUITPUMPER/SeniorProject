8bcb3d657b40f3415050895dc7f52cc8d5595a24
Brian <keyboarddrummer@gmail.com>
2011-04-20 13:47:54 -0400

    Continued Client functionality

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,8 +4,8 @@
 package client;
 
 //Socket Class
-import java.io.IOException;
 import java.net.Socket;
+import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class Client 
@@ -27,5 +27,40 @@ public class Client
 		host = h;
 		port = p;
 		sock = new Socket(host, port);
+	}
+	
+	/**
+	 * Close the current connection to a client
+	 * @throws IOException 
+	 */
+	public void closeConnection() throws IOException
+	{
+		sock.close();
+	}
+	
+	/**
+	 * 
+	 * @param cmd (String) Command to send to server
+	 * @return (boolean) Whether or not the command was sent successfully.  Possible errors include if client isn't connected or if the server responds with an error message
+	 * @throws IOException
+	 */
+	public boolean sendCommand(String cmd) throws IOException
+	{
+		//Assume that the command fails
+		boolean retVal = false;
+		if (sock.isConnected())
+		{
+			sock.getOutputStream().write(cmd.getBytes());
+			retVal = sock.getInputStream().read() < 0;
+		}
+		return retVal;
+	}
+	/**
+	 * Returns the hostname/IP address of the remote host
+	 * @return (String) hostname/IP address of the remote host
+	 */
+	public String getHost()
+	{
+		return host;
 	}
 }
