aeac9a82bb8f790bb2fdb00f32b07dee0787a2c3
Brian <keyboarddrummer@gmail.com>
2011-04-21 10:14:46 -0400

    Added a try-catch for the commandlineclient

--- src/driver/CommandLineClient.java
+++ src/driver/CommandLineClient.java
@@ -1,12 +1,25 @@
 package driver;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Scanner;
+
+import client.Client;
 
 public class CommandLineClient
 {
 	public static void main(String[] args)
 	{
 		Scanner reader = new Scanner(System.in);
-		CommandLineClient client = new CommandLineClient(reader.nextLine(), reader.nextInt());
+		Client client;
+		try 
+		{
+			client = new Client(reader.nextLine(), reader.nextInt());
+		} 
+		catch (Exception e)
+		{
+			System.err.println("Something unexpected has occured!");
+			e.printStackTrace();
+		}
 	}
 }
