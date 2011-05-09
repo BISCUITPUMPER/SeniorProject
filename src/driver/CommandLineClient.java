15fc794722483647d9518b2f2da78f870d2ccd04
Brian <keyboarddrummer@gmail.com>
2011-05-08 20:11:50 -0400

    Added a todo inside of the CommandLineClient

--- src/driver/CommandLineClient.java
+++ src/driver/CommandLineClient.java
@@ -19,6 +19,8 @@ public class CommandLineClient
 			System.err.println("Something unexpected has occured!");
 			e.printStackTrace();
 		}
+		//TODO: Get the following command to send over a socket.
+		//It works when the command is given in the server code, not over a socket (possible a timing error?)
 		client.read();
 		System.out.println("sending message");
 		reader.nextLine();
