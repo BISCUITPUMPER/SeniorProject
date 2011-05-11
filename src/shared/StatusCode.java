714f675297349d9e726570011a13d6004ceea915
Brian <keyboarddrummer@gmail.com>
2011-05-11 10:00:03 -0400

    Added the KEY based enums

--- src/shared/StatusCode.java
+++ src/shared/StatusCode.java
@@ -5,7 +5,9 @@ public enum StatusCode {
 	NO_OPERATION (1),
 	MESSAGE_SENT (2),
 	MOUSE_MOVE (3),
-	MOUSE_SCROLL (4);
+	MOUSE_SCROLL (4),
+	KEY_PRESS (5),
+	KEY_RELEASE (6);
 	
 	public int statusCode;
 	StatusCode(int statusCode)
